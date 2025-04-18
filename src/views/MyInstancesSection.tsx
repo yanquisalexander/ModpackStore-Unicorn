3c7be58506d15f46c0955b8c20756c39e4978909
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2025-04-18 01:19:29 -0300

    feat: ajustar el diseño de la sección de instancias para mejorar la visualización de tarjetas

--- src/views/MyInstancesSection.tsx
+++ src/views/MyInstancesSection.tsx
@@ -46,7 +46,7 @@ export const MyInstancesSection = () => {
                 </p>
             </header>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
                 {instances.map((instance) => (
                     <InstanceCard
                         key={instance.instanceId}

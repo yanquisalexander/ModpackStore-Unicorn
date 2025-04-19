31e8477d97e334f7dfe206c82d0fa3fc34fe42d4
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2025-04-19 13:30:55 -0300

    feat: eliminar el bucle del video en la vista de resumen de modpack

--- src/views/ModpackOverview.tsx
+++ src/views/ModpackOverview.tsx
@@ -179,7 +179,6 @@ export const ModpackOverview = ({ modpackId }: { modpackId: string }) => {
                                 <video
                                     ref={videoRef}
                                     muted
-                                    loop
                                     playsInline
                                     autoPlay
                                     onEnded={handleVideoEnd}

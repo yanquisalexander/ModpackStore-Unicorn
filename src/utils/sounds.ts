693c3f5d6b64de4a3d4c8616e8dfe5e608a849db
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2025-04-19 18:50:54 -0300

    feat: mejorar la gestión de audio al limpiar referencias y pre-cargar sonidos

--- src/utils/sounds.ts
+++ src/utils/sounds.ts
@@ -7,5 +7,23 @@ type SoundKey = keyof typeof SOUNDS;
 export const playSound = (sound: SoundKey, volume: number = 1) => {
     const audio = new Audio(SOUNDS[sound]);
     audio.volume = volume;
+
+    // Limpia la referencia cuando el audio termina
+    audio.addEventListener('ended', () => {
+        // Esto ayuda al recolector de basura a liberar el objeto más rápido
+        audio.src = '';
+        audio.remove(); // Esto solo es necesario si en algún punto se añade al DOM
+    }, { once: true });
+
     audio.play();
+}
+
+export const preloadSounds = () => {
+    Object.values(SOUNDS).forEach((sound) => {
+        const audio = new Audio(sound);
+        audio.load();
+        audio.addEventListener("canplaythrough", () => {
+            console.log(`Sound preloaded: ${sound}`);
+        }, { once: true });
+    });
 }
